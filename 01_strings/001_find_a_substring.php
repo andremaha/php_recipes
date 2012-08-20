6bc080aa075abc2d84e7206ae5df6460909c80f4
Andrey Esaulov <esaulov.andrey@gmail.com>
2012-08-20 12:13:00 +0200

    Initial Commit

---
+++ 01_strings/001_find_a_substring.php
@@ -0,0 +1,13 @@
+<?php
+/**
+*
+* Goal: 		You want to know if the sting contains a substring of a given template
+* 		 		Say, you wish to find out if the email adress was submitted.
+* Solution:		Use the strpos() function. Always use ===, since 0 will mean the beginning of the string.
+*/ 
+
+if (strpos($_GET['email'], '@') === false) {
+	echo 'There was no @ in the e-mail address you have provided!';
+}
+
+echo "You have provided the right e-mail address.";
